33aff131c928737984befacd8d3cb6586baae99b
ekbomp <peter.ekbom@blinfo.se>
2015-12-01 15:16:36 +0100

    Fixat test

--- WindowsApplication3/UnitTestProject1/UnitTest1.vb
+++ WindowsApplication3/UnitTestProject1/UnitTest1.vb
@@ -4,7 +4,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 <TestClass()> Public Class UnitTest1
 
     <TestMethod()> Public Sub TestMethod1()
-        Assert.AreEqual("Kalle", "Nisse")
+        Assert.AreEqual("Kalle", "Kalle")
     End Sub
 
 End Class
