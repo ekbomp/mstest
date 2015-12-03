da871fb5e42fdbdc016d185c123c63212c44ad77
christerstenis <christer.stenis@blinfo.se>
2015-12-03 16:27:37 +0100

    1627

--- WindowsApplication3/WindowsApplication3/Form1.vb
+++ WindowsApplication3/WindowsApplication3/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Label1.Text = "Hej " & TextBox1.Text
     End Sub
 End Class
