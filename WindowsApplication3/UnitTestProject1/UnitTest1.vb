6baec206a3b4a66925672b2d96dbbd839ba4c117
ekbomp <peter.ekbom@blinfo.se>
2015-12-03 10:26:21 +0100

    Test

--- WindowsApplication3/UnitTestProject1/UnitTest1.vb
+++ WindowsApplication3/UnitTestProject1/UnitTest1.vb
@@ -4,7 +4,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 <TestClass()> Public Class UnitTest1
 
     <TestMethod()> Public Sub TestMethod1()
-        Assert.AreEqual("Kalle", "Kalle")
+        Assert.AreEqual("Kalle", "Pelle")
     End Sub
 
 End Class
